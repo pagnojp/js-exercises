22778a004d8e30765d077e34b7a65e641b69828a
João Pedro Pagno <pagno.jp@gmail.com>
2019-07-12 17:42:12 -0300

    exercise 01

---
+++ 01/exer01.js
@@ -0,0 +1,9 @@
+var endereco = {
+    rua: "Rua das árvores",
+    numero: 123,
+    bairro: "Centro",
+    cidade: "Vacaria",
+    uf: "RS"
+};
+
+console.log(`O usuário mora em ${endereco.cidade} / ${endereco.uf}, no bairro ${endereco.bairro}, na rua "${endereco.rua}" com n° ${endereco.numero}.`);
