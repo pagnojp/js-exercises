730ac0a7ba817bcb89fdfe2c96d0f3de3bd60079
João Pedro Pagno <pagno.jp@gmail.com>
2019-07-18 15:44:46 -0300

    add console.error on catch

--- starter/02-es6/exer09.js
+++ starter/02-es6/exer09.js
@@ -2,11 +2,11 @@ const axios = require('axios');
 class User {
   static async getUserInfo(user){
     try {
-    const response = await axios.get(`https://api.github.com/users/${user}`)
-    console.log(response)
+      const response = await axios.get(`https://api.github.com/users/${user}`)
+      console.log(response)
     }
-    catch {
-      console.log('Error')
+    catch (error) {
+      console.error(error)
     }
   }
 }
