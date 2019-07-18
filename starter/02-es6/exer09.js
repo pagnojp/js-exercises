11875651100a346c7612db65b80c34191b4afbc0
João Pedro Pagno <pagno.jp@gmail.com>
2019-07-18 15:48:40 -0300

    exercise 09

--- starter/02-es6/exer09.js
+++ starter/02-es6/exer09.js
@@ -1,13 +1,10 @@
 const axios = require('axios');
-class User {
-  static async getUserInfo(user){
-    try {
-      const response = await axios.get(`https://api.github.com/users/${user}`)
-      console.log(response)
-    }
-    catch (error) {
-      console.error(error)
-    }
+async function getUser(user) {
+  try {
+    const response = await axios.get(`https://api.github.com/users/${user}`)
+    console.log(response)
+  } catch (error) {
+    console.error(error)
   }
 }
-User.getUserInfo('pagnojp')
+getUser('pagnojp')
